4e0a7b617e38a121ddbb4ba4d6834bfc9de9f74a
mattia richetto <mrichetto@opentable.com>
2017-04-06 20:02:05 -0700

    use oc component in example

--- oc-purple-pluto/server.js
+++ oc-purple-pluto/server.js
@@ -1,1 +1,11 @@
-export const data = (context, callback) => callback(null, { staticPath: context.staticPath }); // eslint-disable-line import/prefer-default-export, max-len
+export const data = (context, callback) => { // eslint-disable-line import/prefer-default-export
+  const model = {
+    params: {
+      trackerTarget: context.params.trackerTarget || 'document',
+      painterContainer: context.params.painterContainer || 'document.body',
+    },
+    staticPath: context.staticPath
+  };
+
+  callback(null, model);
+};
